bf315ad3caeeb9ce5984f7a16d1d1a589ead1f79
AshrutSood <ashrut711@gmail.com>
2017-03-30 14:26:44 -0400

    doubled close gear val during shiftUp

--- src/org/usfirst/frc/team4930/robot/commands/ShiftUp.java
+++ src/org/usfirst/frc/team4930/robot/commands/ShiftUp.java
@@ -1,11 +1,15 @@
 package org.usfirst.frc.team4930.robot.commands;
 
 import org.usfirst.frc.team4930.robot.Robot;
+import org.usfirst.frc.team4930.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ShiftUp extends Command
 {
+
+  private double gadgetVal = RobotMap.values.get("close_gadget");
+
   public ShiftUp() {
     requires(Robot.shifter);
   }
@@ -14,13 +18,16 @@ public class ShiftUp extends Command
 
   protected void execute() {
     Robot.shifter.highGear();
+    RobotMap.values.put("close_gadget", gadgetVal * 2);
   }
 
   protected boolean isFinished() {
     return true;
   }
 
-  protected void end() {}
+  protected void end() {
+    RobotMap.values.put("close_gadget", gadgetVal);
+  }
 
   protected void interrupted() {
     end();
--- src/org/usfirst/frc/team4930/robot/utilities/Dashboard.java
+++ src/org/usfirst/frc/team4930/robot/utilities/Dashboard.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4930.robot.utilities;
 
 import org.usfirst.frc.team4930.robot.Robot;
 import org.usfirst.frc.team4930.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dashboard
@@ -24,17 +25,24 @@ public class Dashboard
       Robot.dial.setSelectedReplay();
 
       // put hard code values
-      SmartDashboard.putString("1", "Agitate: " + String.valueOf(RobotMap.values.get("agitate") * 100));
-      SmartDashboard.putString("2", "Loader: " + String.valueOf(RobotMap.values.get("loader") * 100));
-      SmartDashboard.putString("3", "Shooter: " + String.valueOf(RobotMap.values.get("shooter") * 100));
-      SmartDashboard.putString("4", "Intake: " + String.valueOf(RobotMap.values.get("intake_in") * 100));
-      SmartDashboard.putString("5", "Unload: " + String.valueOf(RobotMap.values.get("intake_out") * 100));
-      SmartDashboard.putString("6", "Clear: " + String.valueOf(RobotMap.values.get("clear_fuel") * 100));
+      SmartDashboard.putString("1",
+          "Agitate: " + String.valueOf(RobotMap.values.get("agitate") * 100));
+      SmartDashboard.putString("2",
+          "Loader: " + String.valueOf(RobotMap.values.get("loader") * 100));
+      SmartDashboard.putString("3",
+          "Shooter: " + String.valueOf(RobotMap.values.get("shooter") * 100));
+      SmartDashboard.putString("4",
+          "Intake: " + String.valueOf(RobotMap.values.get("intake_in") * 100));
+      SmartDashboard.putString("5",
+          "Unload: " + String.valueOf(RobotMap.values.get("intake_out") * 100));
+      SmartDashboard.putString("6",
+          "Clear: " + String.valueOf(RobotMap.values.get("clear_fuel") * 100));
 
       // put sensor values
       SmartDashboard.putString("7", "Gyro Angle: " + String.valueOf(Robot.gyro.getAngle()));
       SmartDashboard.putString("8", "Left Encoder: " + String.valueOf(Robot.encoders.leftPosRaw()));
-      SmartDashboard.putString("9", "Right Encoder: " + String.valueOf(Robot.encoders.rightPosRaw()));
+      SmartDashboard.putString("9",
+          "Right Encoder: " + String.valueOf(Robot.encoders.rightPosRaw()));
       SmartDashboard.putString("10", "Accel X: " + String.valueOf(Robot.accel.getX()));
       SmartDashboard.putString("11", "Accel Y: " + String.valueOf(Robot.accel.getY()));
       SmartDashboard.putString("12", "Accel Z: " + String.valueOf(Robot.accel.getZ()));
@@ -47,6 +55,7 @@ public class Dashboard
       SmartDashboard.putString("17", "Alliance Switch: " + Robot.switches.getAlliance());
       SmartDashboard.putString("18", "Replay Switch: " + Robot.switches.getRoutineOrReplay());
 
+      SmartDashboard.putString("19", "Gear Gadget Value: " + String.valueOf(RobotMap.values.get("close_gadget") * 100));
     }
 
   }
